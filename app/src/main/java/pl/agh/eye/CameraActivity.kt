47e24408ce502a4e9351b5fd84e4d3c956de9283
Marcin Majewski <majewskimarcin@outlook.com>
2020-03-25 12:24:44 +0100

    * Adding camera activity * First fragment triggers camera activity

---
+++ app/src/main/java/pl/agh/eye/CameraActivity.kt
@@ -0,0 +1,143 @@
+package pl.agh.eye
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.SurfaceView
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.opencv.android.*
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
+
+
+class CameraActivity : AppCompatActivity(), CvCameraViewListener2 {
+    // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
+    private var mOpenCvCameraView: CameraBridgeViewBase? = null
+    // Used in Camera selection from menu (when implemented)
+    private val mIsJavaCamera = true
+    private val mItemSwitchCamera: MenuItem? = null
+    // These variables are used (at the moment) to fix camera orientation from 270degree to 0degree
+    var mRgba: Mat? = null
+    var mRgbaF: Mat? = null
+    var mRgbaT: Mat? = null
+    private val mLoaderCallback: BaseLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    Log.i(
+                        TAG,
+                        "OpenCV loaded successfully"
+                    )
+                    mOpenCvCameraView!!.enableView()
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "called onCreate")
+        super.onCreate(savedInstanceState)
+        checkAndRequestPermissions()
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setContentView(R.layout.show_camera)
+        mOpenCvCameraView =
+            findViewById<View>(R.id.show_camera_activity_java_surface_view) as JavaCameraView
+        mOpenCvCameraView!!.visibility = SurfaceView.VISIBLE
+        mOpenCvCameraView!!.setCvCameraViewListener(this)
+        mOpenCvCameraView!!.enableView()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        if (mOpenCvCameraView != null) mOpenCvCameraView!!.disableView()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(
+                TAG,
+                "Internal OpenCV library not found. Using OpenCV Manager for initialization"
+            )
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback)
+        } else {
+            Log.d(
+                TAG,
+                "OpenCV library found inside package. Using it!"
+            )
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS)
+        }
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        if (mOpenCvCameraView != null) mOpenCvCameraView!!.disableView()
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        mRgba = Mat(height, width, CvType.CV_8UC4)
+        mRgbaF = Mat(height, width, CvType.CV_8UC4)
+        mRgbaT = Mat(width, width, CvType.CV_8UC4)
+        Log.i(TAG, "camera view started")
+    }
+
+    override fun onCameraViewStopped() {
+        mRgba!!.release()
+        Log.i(TAG, "camera view stopped")
+    }
+
+    override fun onCameraFrame(inputFrame: CvCameraViewFrame): Mat { // TODO Auto-generated method stub
+        mRgba = inputFrame.rgba()
+        // Rotate mRgba 90 degrees
+        Core.transpose(mRgba, mRgbaT)
+        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF!!.size(), 0.0, 0.0, 0)
+        Core.flip(mRgbaF, mRgba, 1)
+
+        return mRgba as Mat// This function must return
+    }
+
+    companion object {
+        // Used for logging success or failure messages
+        private const val TAG = "OCVSample::Activity"
+    }
+
+    init {
+        Log.i(
+            TAG,
+            "Instantiated new " + this.javaClass
+        )
+    }
+
+    private fun checkAndRequestPermissions(): Boolean {
+        val camera = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        )
+        val listPermissionsNeeded: MutableList<String> = ArrayList()
+        if (camera != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA)
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                listPermissionsNeeded.toTypedArray(),
+                7
+            )
+            return false
+        }
+        return true
+    }
+}
--- app/src/main/java/pl/agh/eye/FirstFragment.kt
+++ app/src/main/java/pl/agh/eye/FirstFragment.kt
@@ -1,17 +1,19 @@
 package pl.agh.eye
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(){
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +26,12 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val myIntent = Intent(activity, CameraActivity::class.java)
+        startActivity(myIntent)
+
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
+
 }
